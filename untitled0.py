f6332eb0b5140cfb75a487042bb226e232608de2
Sanan <asannan822@gmail.com>
2019-09-08 22:47:26 +0530

    first upload to track hand
    Hand recoginition

---
+++ untitled0.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep  7 22:58:22 2019
+
+@author: Ansari
+"""
+import cv2
+if __name__ == "__main__":
+    # initialize weight for running average
+    aWeight = 0.5
+
+    # get the reference to the webcam
+    camera = cv2.VideoCapture('gesture.mp4')
+
+    # region of interest (ROI) coordinates
+    top, right, bottom, left = 10, 350, 225, 590
+
+    # initialize num of frames
+    num_frames = 0
+
+    # keep looping, until interrupted
+    while(True):
+        # get the current frame
+        (grabbed, frame) = camera.read()
+
+        # resize the frame
+        frame = cv2.resize(frame, width=700,hieght=600)
+
+        # flip the frame so that it is not the mirror view
+        frame = cv2.flip(frame, 1)
+
+        # clone the frame
+        clone = frame.copy()
+
+        # get the height and width of the frame
+        (height, width) = frame.shape[:2]
+
+        # get the ROI
+        roi = frame[top:bottom, right:left]
+
+        # convert the roi to grayscale and blur it
+        gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+        gray = cv2.GaussianBlur(gray, (7, 7), 0)
+
+        # to get the background, keep looking till a threshold is reached
+        # so that our running average model gets calibrated
+        if num_frames < 30:
+            run_avg(gray, aWeight)
+        else:
+            # segment the hand region
+            hand = segment(gray)
+
+            # check whether hand region is segmented
+            if hand is not None:
+                # if yes, unpack the thresholded image and
+                # segmented region
+                (thresholded, segmented) = hand
+
+                # draw the segmented region and display the frame
+                cv2.drawContours(clone, [segmented + (right, top)], -1, (0, 0, 255))
+                thresholded = cv2.resize(thresholded, (800, 700))
+                cv2.imshow("Thesholded", thresholded)
+
+        # draw the segmented hand
+        cv2.rectangle(clone, (left, top), (right, bottom), (0,255,0), 2)
+
+        # increment the number of frames
+        num_frames += 1
+
+        # display the frame with segmented hand
+        clone = cv2.resize(clone, (800, 700))
+        cv2.imshow("Video Feed", clone)
+
+        # observe the keypress by the user
+        keypress = cv2.waitKey(1) & 0xFF
+
+        # if the user pressed "q", then stop looping
+        if keypress == ord("q"):
+            break
+
+# free up memory
+cv2.destroyAllWindows()
---
+++ videoread.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep  7 11:42:20 2019
+
+@author: Ansari
+"""
+import cv2
+import numpy as pp
+vidcap = cv2.VideoCapture('gesture.mp4')
+success,image = vidcap.read()
+count = 0
+while success:
+  cv2.imwrite("frame%d.jpg" % count, image)     # save frame as JPEG file      
+  success,image = vidcap.read()
+  print('Read a new frame: ', success)
+  count += 1
+  if count%10==0:
+      image1 = cv2.transpose(image,image)
+      image1 = cv2.flip(image1,0 )
+      imS = cv2.resize(image1, (800, 700))
+      roi=imS[50:200, 600:600]
+      r = cv2.rectangle(imS,(50,200),(700,700),(0,255,0),0) 
+      imS=cv2.cvtColor(imS, cv2.COLOR_BGR2HSV)
+      #cv2.imshow('frame',imS)
+      l = pp.array([0, 50, 80], dtype = "uint8")
+      u = pp.array([23, 255, 255], dtype = "uint8")
+      skinDetect = cv2.inRange(imS, l, u)
+      #print(l,u,skinDetect[0])
+      skinDetect = cv2.GaussianBlur(skinDetect,(5,5),100)
+      cv2.imshow('Masked Image',skinDetect)
+      #cv2.imshow('frame',imS)
+      frame =  cv2.bitwise_and(imS,imS, mask= skinDetect)
+      #cv2.imshow('frame',frame)
+      cv2.waitKey(0) 
+      #break
+cv2.destroyAllWindows()
